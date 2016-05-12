9706bc625e16b5660db8b98dcf5707019559d1ba
emilio <emsmnndz@gmail.com>
2016-05-12 12:06:12 +0100

    added functionality to reset and toggle tempereature

--- interface.js
+++ interface.js
@@ -11,4 +11,14 @@ $(document).ready(function() {
     thermostat.decreaseTemperature();
     $('#temperature').text(thermostat.currentTemperature());
   });
+
+  $('#reset').on('click', function(){
+    thermostat.resetTemperature();
+    $('#temperature').text(thermostat.currentTemperature());
+  });
+
+  $('#PSM').on('click', function() {
+    thermostat.switchPowerSavingMode();
+    $('#psm_status').text(thermostat.isPowerSavingModeOn());
+  });
 });
