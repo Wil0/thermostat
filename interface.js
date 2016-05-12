63dc6eba8abd87bcf8fbb42971b5a19b67bffc1a
Alex Robertson <lex1989@gmail.com>
2016-05-12 14:00:23 +0100

    added weather api

--- interface.js
+++ interface.js
@@ -28,4 +28,11 @@ $(document).ready(function() {
     $('#temperature').text(thermostat.currentTemperature());
      $('#screen').css('background', thermostat.colour());
   }
+  $('#city').change(function() {
+    var city = $('#city').val();
+
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function(data) {
+    $('#temp_outside').text(data.main.temp);
+  });
+  });
 });
